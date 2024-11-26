97888d32ddb3160e0d60cd6e4f1ad8b01015b1d0
adanguerra1990 <aj90guerra@gmail.com>
2024-11-26 10:11:05 -0400

    Hook personalizado para simplificar es uso del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   useParams,
   useNavigate,
 } from 'react-router-dom'
+import { useField } from './hooks/useField'
 
 const Menu = () => {
   const padding = {
@@ -93,18 +94,18 @@ const Footer = () => (
   </Router>
 )
 
-const CreateNew = props => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+const CreateNew = ({ addNew }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = e => {
     e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
+    addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
     navigate('/')
@@ -116,29 +117,18 @@ const CreateNew = props => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={e => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={e => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={e => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button>reset</button>
       </form>
     </div>
   )
